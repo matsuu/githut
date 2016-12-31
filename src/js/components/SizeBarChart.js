d4c3d38a472d0660d03baba5e850bb1d3976016b
Fabian Beuke <mail@beuke.org>
2016-12-31 20:20:27 +0100

    add avg lang size chart

--- src/js/components/SizeBarChart.js
+++ src/js/components/SizeBarChart.js
@@ -3,22 +3,25 @@ import axios from 'axios'
 import { observer } from 'mobx-react'
 import avgsize from '../../data/gh-avg-repo-size.json'
 import ReactHighcharts from 'react-highcharts'
-import { mapKeys, sortBy, filter, includes, update, take, map, split, first } from 'lodash/fp'
+import { mapKeys, each, sortBy, reverse, filter, includes, update, take, map, split, first } from 'lodash/fp'
 
 @observer
 export default class SizeBarChart extends React.Component {
 
     constructor() {
-        super()
-        this.config = {
+    super()
+    this.config = {
         credits: { enabled: false },
         chart: { type: 'column', backgroundColor: 'transparent' },
-        title: { text: 'Average Repository Size' },
-        xAxis: {
-        categories: [],
-        }
+        title: { text: 'Average Language Sizes per Repository' },
+        plotOptions: { column: { colorByPoint: true } },
+        xAxis: { categories: [] },
+        yAxis: {
+            type: 'logarithmic',
+            minorTickInterval: 'auto'
         }
     }
+}
 
     topLanguagesReady() {
         return this.props.store.topLanguages.length >= 10
@@ -30,31 +33,21 @@ export default class SizeBarChart extends React.Component {
     async componentWillUpdate() {
         if (!this.topLanguagesReady())
             return
-
         const { data } = await axios.get(avgsize)
         const chart = this.refs.chart.getChart()
         const { getTopLanguages } = this.props.store
-        const t = getTopLanguages.sort()
+        const topLang = getTopLanguages.sort()
         const size = data
-        | split('\n')
-        | map(JSON.parse)
-        | map(update('avg_kb_size_per_repo')(Math.floor))
-        | filter(o => includes(o.lang)(t))
-        | sortBy('lang')
-        | map('avg_kb_size_per_repo')
-        | take(10)
-        console.log(size)
-        // first(chart.xAxis).setCategories(this.categories())
-        getTopLanguages.map( x => {
-            if (x && this.config.xAxis.categories.length < 10) {
-                this.config.xAxis.categories.push(x)
-                const chart = this.refs.chart.getChart()
-                first(chart.xAxis).setCategories(this.config.xAxis.categories.sort())
-                // console.log(x)
-                // console.log(this.config.xAxis.categories)
-                chart.redraw()
-            }
-        } )
+            | split('\n')
+            | map(JSON.parse)
+            | map(update('avg_kb_size_per_repo')(Math.floor))
+            | filter(o => includes(o.lang)(topLang))
+            | sortBy('avg_kb_size_per_repo')
+            | reverse
+            | each(o => this.config.xAxis.categories.push(o.lang) )
+            | map('avg_kb_size_per_repo')
+            | take(10)
+        first(chart.xAxis).setCategories(this.config.xAxis.categories)
         chart.addSeries({ data: size }, false)
         chart.redraw()
     }
