04c801cb8832ff557132d97432db64c236babe82
Fabian Beuke <mail@beuke.org>
2017-01-31 00:40:06 +0100

    add wiki api lang info fetch with webuipopover

--- src/js/client.js
+++ src/js/client.js
@@ -5,7 +5,23 @@ import styles from '../style.styl' // eslint-disable-line no-unused-vars
 import 'jquery/dist/jquery.min.js' // Material Button
 import 'materialize-css/bin/materialize.js' // Style
 
+import 'webui-popover/dist/jquery.webui-popover.min.js'
+
 const app = document.createElement('div')
 app.id = "app"
 document.body.appendChild(app);
 ReactDOM.render(<Layout/>, app)
+
+// $('a').webuiPopover({title:'Title',content:'Content'});
+
+$('a').webuiPopover({
+                        type:'async',
+                        width: '500',
+                        height: '400',
+                        url:'https://en.wikipedia.org/w/api.php?action=query&prop=extracts&format=json&exintro=&origin=*&titles=JavaScript',
+                        content:function(data){
+                            console.log(data)
+                            console.log(data.query.pages[9845].extract)
+                            return data.query.pages[9845].extract;
+                        }
+                    });
--- src/js/components/Head.js
+++ src/js/components/Head.js
@@ -13,7 +13,8 @@ export default function Application () {
                  {'rel': 'stylesheet', 'type': 'text/css', 'href': '//fonts.googleapis.com/icon?family=Material+Icons'},
                  {'rel': 'stylesheet', 'type': 'text/css', 'href': '//allenfang.github.io/react-bootstrap-table/css/react-bootstrap-table-all.min.css'},
                  {'rel': 'stylesheet', 'type': 'text/css', 'href': '//cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css'},
-                 {'rel': 'stylesheet', 'type': 'text/css', 'href': '//fonts.googleapis.com/css?family=Open+Sans:400,300,700'}
+                 {'rel': 'stylesheet', 'type': 'text/css', 'href': '//fonts.googleapis.com/css?family=Open+Sans:400,300,700'},
+                 {'rel': 'stylesheet', 'type': 'text/css', 'href': '//cdn.jsdelivr.net/jquery.webui-popover/1.2.1/jquery.webui-popover.min.css'}
                 ]}
              />
         </div>
